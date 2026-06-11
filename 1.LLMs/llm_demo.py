a898e5dd7934644615888f2c0811c946cbe1e764
Bhupendra Shivhare <149234458+shivharebhupendra@users.noreply.github.com>
2026-06-11 11:17:47 +0530

    Deleting the old files and adding the new files to the staging area for commit. The commit message should reflect the changes made, such as "Refactor project structure and update file locations".

---
+++ 1.LLMs/llm_demo.py
@@ -0,0 +1,13 @@
+# Open AI is Paid Service
+
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = OpenAI(model = "gpt-4.1-mini")
+
+result = llm.invoke("What is the capital of India.")
+
+print(result)
+
